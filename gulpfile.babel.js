c53437ca37fbaa97e2190dc8f80a4280970fd57c
Beckwith <beckwmi2@localhost.na.novartis.net>
2015-08-29 19:22:47 -0400

    gulp all the things

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,8 +10,10 @@ import buffer from 'vinyl-buffer';
 import gutil from 'gulp-util';
 import sourcemaps from 'gulp-sourcemaps';
 import assign from 'object-assign';
+import browserSync from 'browser-sync';
 import babelify from 'babelify';
 import reactify from 'reactify';
+import sass from 'gulp-sass';
 
 // add custom browserify options here
 const customOpts = {
@@ -44,4 +46,25 @@ const bundle = () => {
 b.on('update', bundle); // on any dep update, runs the bundler
 b.on('log', gutil.log); // output build logs to terminal
 
+gulp.task('bundle', bundle);
 gulp.task('default', bundle);
+gulp.task('sass', () => {
+    gulp.src('src/sass/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
+gulp.task('watch', () => {
+    gulp.watch('src/sass/*.sass', ['sass']);
+    gulp.watch('src/css/*.css', ['css']);
+});
+gulp.task('serve', () => {
+    browserSync({
+        port: 3000,
+        files: ['dist/js/bundle.js', 'dist/css/*.css'],
+        server: {
+            baseDir: './'
+        }
+    });
+});
+
+gulp.task('default', ['watch', 'sass', 'bundle', 'serve']);
--- src/js/dispatcher/AppDispatcher.js
+++ src/js/dispatcher/AppDispatcher.js
@@ -15,10 +15,6 @@ class AppDispatcher extends Dispatcher {
 
 };
 
-// kinda annoying that the same dispatcher instance needs to be shared
-// wonder if there's a way to make something "static"
-// eh kinda but not really 
-// http://stackoverflow.com/questions/22528967/es6-class-variable-alternatives
 // TODO think about smarter ways to do this
 var appDispatcher = new AppDispatcher;
 
